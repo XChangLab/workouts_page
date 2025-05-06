167a446304911a04577aaecbc35605232877753a
Jeff Wen <sinchangwen@gmail.com>
2025-05-06 10:10:52 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,8 +17,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteUrl: 'https://sinchang.me',
+  logo: 'https://avatars.githubusercontent.com/u/3297859?v=4',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
@@ -26,14 +26,14 @@ const data: ISiteMetadataResult = {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
     },
-    {
-      name: 'Blog',
-      url: 'https://ben29.xyz',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
-    },
+    // {
+    //   name: 'Blog',
+    //   url: 'https://ben29.xyz',
+    // },
+    // {
+    //   name: 'About',
+    //   url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+    // },
   ],
 };
 
