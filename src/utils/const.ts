f2b75767e2c1e785505492883fe0d6848fec8b93
Jeff Wen <sinchangwen@gmail.com>
2025-05-28 09:50:55 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -167,7 +167,7 @@ export {
 
 const nike = 'rgb(224,237,94)';
 const yellow = 'rgb(224,237,94)';
-const green = 'rgb(0,237,94)';
+const green = '#FC5200';
 const pink = 'rgb(237,85,219)';
 const cyan = 'rgb(112,243,255)';
 const IKB = 'rgb(0,47,167)';
